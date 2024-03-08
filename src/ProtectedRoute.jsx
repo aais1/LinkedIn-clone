a601327773da9ad86aae89b52a9bfd665550a4e2
AliAais <aaisali228@gmail.com>
2024-03-07 23:23:35 -0800

    Revert "fixing route protection"
    This reverts commit 16266b70a50784e2aee700cb35943981e3f3be6a.

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,24 +1,15 @@
-import { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
 
 const ProtectedRoute = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const location = useLocation();
+  let location = useLocation();
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUser(user);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+  });
   return <>{children}</>;
 };
 
