16266b70a50784e2aee700cb35943981e3f3be6a
AliAais <aaisali228@gmail.com>
2024-03-07 23:22:00 -0800

    fixing route protection

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,15 +1,24 @@
+import { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
 
 const ProtectedRoute = ({ children }) => {
-  let location = useLocation();
+  const [user, setUser] = useState(null);
+  const location = useLocation();
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      return <Navigate to="/login" state={{ from: location }} replace />;
-    }
-  });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
   return <>{children}</>;
 };
 
